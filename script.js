eea5b0374157cb90790efd87c3bb3a80cb7fa4be
ThallysCezar <75546628+ThallysCezar@users.noreply.github.com>
2021-12-19 21:33:48 -0300

    Refactor: js and html

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ function insert(num)
     var numero = document.getElementById('resultado').innerHTML;
     document.getElementById('resultado').innerHTML = numero + num;
 }
+
+function clean()
+{
+    document.getElementById('resultado').innerHTML = "";
+}
+
+function back()
+{
+    var resultado = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1)
+}
